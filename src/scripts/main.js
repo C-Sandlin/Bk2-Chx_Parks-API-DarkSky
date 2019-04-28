7519016def6840c7fcfc9ca7ee793091507de0c4
Colin Sandlin <csandlin010@gmail.com>
2019-04-28 12:48:19 -0500

    refactored fetch functions to reduce and consolidate code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 //
+const parksPublishLocations = document.querySelector("#place-parks-list-here");
 
 function getParksData() {
   fetch(
@@ -7,77 +8,33 @@ function getParksData() {
     .then(parks => parks.json())
     .then(parsedParks => {
       console.table(parsedParks);
-      for (var i = 0; i < 10; i++) {
-        let parkName = parsedParks.parks[i].name;
-        let parkLocation = parsedParks.parks[i].state;
-        let visitStatus = parsedParks.parks[i].visited;
-        let lat = parsedParks.parks[i].latitude;
-        let long = parsedParks.parks[i].longitude;
-        getWeatherData(parkName, parkLocation, visitStatus, lat, long);
-      }
+      parsedParks.parks.forEach(park => {
+        fetch(
+          `https://api.darksky.net/forecast/${key}/${park.latitude},${
+            park.longitude
+          }`
+        )
+          .then(weather => weather.json())
+          .then(parsedWeather => {
+            let currently = parsedWeather.currently.summary;
+            let hourlySum = parsedWeather.hourly.summary;
+            let weekSum = parsedWeather.daily.summary;
+
+            const parkHTML = `
+              <article class="visit-status-${park.visited}">
+              <h3>${park.name}</h3>
+              <p>${park.state}</p>
+              <ul>
+              <li>Currently: ${currently}</li>
+              <li>Today: ${hourlySum}</li>
+              <li>Week: ${weekSum}</li>
+              </ul>
+              </article>
+              `;
+            parksPublishLocations.innerHTML += parkHTML;
+          });
+      });
     });
 }
 
 getParksData();
-
-function getWeatherData(parkName, parkLocation, visitStatus, lat, long) {
-  fetch(`https://api.darksky.net/forecast/${key}/${lat},${long}`)
-    .then(weather => weather.json())
-    .then(parsedWeather => {
-      let currently = parsedWeather.currently.summary;
-      let hourlySum = parsedWeather.hourly.summary;
-      let weekSum = parsedWeather.daily.summary;
-      publishToDom(
-        parkName,
-        parkLocation,
-        visitStatus,
-        currently,
-        hourlySum,
-        weekSum
-      );
-    });
-}
-
-function publishToDom(
-  parkName,
-  parkLocation,
-  visitStatus,
-  currently,
-  hourlySum,
-  weekSum
-) {
-  const parksPublishLocations = document.querySelector(
-    "#place-parks-list-here"
-  );
-  const makeArt = document.createElement("article");
-  const makeH3 = document.createElement("h3");
-  const makeP = document.createElement("p");
-  const makeP2 = document.createElement("p");
-  const makeUl = document.createElement("ul");
-  const makeLi = document.createElement("li");
-  const makeLi2 = document.createElement("li");
-  const makeLi3 = document.createElement("li");
-
-  makeH3.textContent = `${parkName}`;
-  makeP.textContent = `Located in the state of ${parkLocation}`;
-  makeP2.textContent = "Weather:";
-  makeLi.textContent = `Currently: ${currently}`;
-  makeLi2.textContent = `Today: ${hourlySum}`;
-  makeLi3.textContent = `This Week: ${weekSum}`;
-
-  parksPublishLocations.appendChild(makeArt);
-  makeArt.appendChild(makeH3);
-  makeArt.appendChild(makeP);
-  makeArt.appendChild(makeP2);
-  makeArt.appendChild(makeUl);
-  makeUl.appendChild(makeLi);
-  makeUl.appendChild(makeLi2);
-  makeUl.appendChild(makeLi3);
-
-  if (visitStatus === true) {
-    makeArt.classList.add("visited");
-  }
-  if (visitStatus === false) {
-    makeArt.classList.add("not-visited");
-  }
-}
